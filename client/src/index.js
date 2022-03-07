51eb703f89590bc2171862a8dbdc12aadd939adb
MiloszSzymanek <szymanekmilosz92@gmail.com>
2022-03-07 11:58:22 +0100

    errors solved

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 import SocialHouseContextProvider from './Context';
 import Routes from './Routes';
 
