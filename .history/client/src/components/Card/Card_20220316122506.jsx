dc1b6c6ca49a55f8e402dbd980a2b2d971e3ca5d
esrkkn <esrakkan.ea@gmail.com>
2022-03-16 13:58:33 +0100

    "commited"

---
+++ .history/client/src/components/Card/Card_20220316122506.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import {
+  MDBCard,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardBody,
+  MDBCardImage,
+  MDBRow,
+  MDBCol,
+} from "mdb-react-ui-kit";
+
+export default function Card({ item }) {
+  const { userData } = useContext(SocialHouseContext);
+
+  return (
+    <MDBCard style={{ maxWidth: "540px" }}>
+      <MDBRow className="g-0">
+        <MDBCol md="4">
+          <MDBCardImage src={item.image} alt="..." fluid />
+        </MDBCol>
+        <MDBCol md="8">
+          <MDBCardBody>
+            <MDBCardTitle>{item.title}</MDBCardTitle>
+            <MDBCardText>{item.description}</MDBCardText>
+            <MDBCardText>{item.location}</MDBCardText>
+
+            <MDBCardText>
+              <span>{item.rooms}</span>
+              <span>{item.price}</span>
+            </MDBCardText>
+          </MDBCardBody>
+        </MDBCol>
+      </MDBRow>
+    </MDBCard>
+  );
+}
